8eae2df0095cb98c51bbc41a1237bb590ed1c953
Yagiz Nizipli <yagiz@nizipli.com>
2021-06-20 12:08:36 +0300

    build: prepare github workflow

--- db/globals.sql
+++ db/globals.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION btree_gist;
 CREATE ROLE tracking;
-CREATE ROLE "tracking-worker" LOGIN PASSWORD 'change-this';
+CREATE ROLE "tracking-worker" LOGIN PASSWORD 'tracking-password';
 
 CREATE DATABASE tracking OWNER tracking TEMPLATE template0 ENCODING "UTF-8" LC_COLLATE "C" LC_CTYPE "C";
