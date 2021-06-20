07349cb0d149bc96e24f349d1f0db83dcf1f15b8
Emre DEĞER <ben@emre.dev>
2021-06-20 14:08:43 +0300

    chore: added migrations

---
+++ db/migrations/20210620102725_applications.cjs
@@ -0,0 +1,26 @@
+exports.up = function (knex) {
+  return knex.schema.raw(`
+    CREATE TABLE applications (
+      account_id uuid NOT NULL,
+      application_id text NOT NULL,
+      title text NOT NULL,
+      authorization_key bytea NOT NULL,
+      
+      application_key bytea NOT NULL,
+      server_key bytea NOT NULL,
+      
+      session_timeout int NOT NULL DEFAULT 30, -- 30 minutes
+      is_active bool NOT NULL DEFAULT true,
+      created_at timestamptz NOT NULL DEFAULT NOW(),
+      updated_at timestamptz NOT NULL DEFAULT NOW(),
+    
+      PRIMARY KEY (account_id, application_id),
+      
+      CHECK (session_timeout >= 30)
+    );
+  `)
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('applications')
+}
