0e542f767afa8bf7b5cbec5811d1041d78a8fc6e
Emre DEĞER <ben@emre.dev>
2021-06-20 15:27:05 +0300

    fix: removed old sql schema's

--- db/globals.sql
+++
@@ -1,4 +0,0 @@
-CREATE ROLE tracking;
-CREATE ROLE "tracking-worker" LOGIN PASSWORD 'tracking-password';
-
-CREATE DATABASE tracking OWNER tracking TEMPLATE template0 ENCODING "UTF-8" LC_COLLATE "C" LC_CTYPE "C";
