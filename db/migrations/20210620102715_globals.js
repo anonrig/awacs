d5986cfa9bd8686c29b59408119499b9088cb7d1
Yagiz Nizipli <yagiz@nizipli.com>
2021-06-21 11:18:55 +0300

    perf: improve helm chart

--- db/migrations/20210620102715_globals.js
+++ db/migrations/20210620102715_globals.js
@@ -1,11 +1,7 @@
 export function up(knex) {
-  return knex.schema.raw(`
-    CREATE EXTENSION IF NOT EXISTS btree_gist;
-  `)
+  return knex.schema.raw(`CREATE EXTENSION IF NOT EXISTS btree_gist;`)
 }
 
 export function down(knex) {
-  return knex.schema.raw(`
-    DROP EXTENSION IF EXISTS btree_gist;
-  `)
+  return knex.schema.raw(`DROP EXTENSION IF EXISTS btree_gist;`)
 }
