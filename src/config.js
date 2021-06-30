048b902cd93d921d2c09aa3879e9ec7840e81733
Yagiz Nizipli <yagiz@nizipli.com>
2021-06-30 15:31:46 +0300

    refactor: remove unused sentry config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ export default {
   isProduction: process.env.NODE_ENV === 'production',
   port: process.env.PORT ? parseInt(process.env.PORT) : 3002,
   grpc_port: process.env.GRPC_PORT ? parseInt(process.env.GRPC_PORT) : 4001,
-  sentry: process.env.SENTRY_DSN,
   knex: {
     client: 'pg',
     version: '13',
