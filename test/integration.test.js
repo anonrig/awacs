e04ebd67dc2ca7fc2636c020bbe81f2921c5f479
Yagiz Nizipli <yagiz@nizipli.com>
2021-06-30 11:11:29 +0300

    test: properly test in_app_purchase event

--- test/integration.test.js
+++ test/integration.test.js
@@ -113,6 +113,39 @@ test('public api should accept set_client requests', async (t) => {
   t.is(updated_client.row.distinct_id, 'hello-world-from-integration-tests')
 })
 
+test('public api should accept in_app_purchase requests', async (t) => {
+  const client_id = v4()
+  const { application, account_id } = t.context
+  const payload = [
+    {
+      name: 'app_open',
+      timestamp: dayjs().subtract(1, 'days').unix() * 1000,
+      ...app_open,
+    },
+    {
+      name: 'in_app_purchase',
+      timestamp: dayjs().subtract(12, 'hours').unix() * 1000,
+      product_name: 'Weekly Package',
+      product_quantity: 1,
+      product_price: 4.99,
+      product_currency: 'USD',
+    },
+  ]
+
+  await sendEventRequest(application, client_id, payload)
+
+  const Events = promisifyAll(t.context.clients.Events)
+  const events = await Events.findAll({ account_id, client_id })
+  const event = events.rows.find((r) => r.title === 'in_app_purchase')
+  t.truthy(event)
+  t.deepEqual(event.properties, [
+    { key: 'product_name', value: 'Weekly Package' },
+    { key: 'product_price', value: '4.99' },
+    { key: 'product_currency', value: 'USD' },
+    { key: 'product_quantity', value: '1' },
+  ])
+})
+
 test('public api should accept custom requests', async (t) => {
   const client_id = v4()
   const { application, account_id } = t.context
@@ -123,7 +156,7 @@ test('public api should accept custom requests', async (t) => {
       ...app_open,
     },
     {
-      name: 'say_hi',
+      name: 'custom',
       timestamp: dayjs().subtract(12, 'hours').unix() * 1000,
       message: 'hello from the other side',
     },
@@ -133,5 +166,9 @@ test('public api should accept custom requests', async (t) => {
 
   const Events = promisifyAll(t.context.clients.Events)
   const events = await Events.findAll({ account_id, client_id })
-  t.truthy(events.rows.find((r) => r.title === 'say_hi'))
+  t.truthy(
+    events.rows[0].properties.find(
+      (r) => r.key === 'message' && r.value === 'hello from the other side',
+    ),
+  )
 })
