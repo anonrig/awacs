5b47837a5484e463fff7c74dffda4ed668d41976
Yagiz Nizipli <yagiz@nizipli.com>
2021-07-09 18:02:07 +0300

    test: add tests for events pagination

--- test/grpc/events.test.js
+++ test/grpc/events.test.js
@@ -1,7 +1,14 @@
 import test from 'ava'
 import { randomUUID } from 'crypto'
-import { getRandomPort, getGrpcClients } from '../helper.js'
+import dayjs from 'dayjs'
+import {
+  getRandomPort,
+  getGrpcClients,
+  sendEventRequest,
+  promisifyAll,
+} from '../helper.js'
 import server from '../../src/grpc.js'
+import { app_open } from '../seeds.js'
 
 test.before(async (t) => {
   t.context.server = server
@@ -40,4 +47,58 @@ test.cb('findAll should validate account_id', (t) => {
     t.is(error.message, '9 FAILED_PRECONDITION: Invalid account_id')
     t.end()
   })
+})
+
+test('findAll should paginate with limit', async (t) => {
+  const Events = promisifyAll(t.context.clients.Events)
+  const Applications = promisifyAll(t.context.clients.Applications)
+  const account_id = randomUUID()
+  const application_id = randomUUID()
+
+  await Applications.create({
+    account_id,
+    application_id,
+    title: 'Test Application',
+    session_timeout: 60,
+  })
+
+  const { row: application } = await Applications.findOne({
+    account_id,
+    application_id,
+  })
+
+  const clients = [randomUUID(), randomUUID()]
+
+  await sendEventRequest(application, clients[0], [
+    {
+      name: 'app_open',
+      timestamp: dayjs().subtract(1, 'day').unix() * 1000,
+      ...app_open,
+    },
+  ])
+  await sendEventRequest(application, clients[1], [
+    { name: 'app_open', timestamp: Date.now(), ...app_open },
+  ])
+
+  const { rows: first_page, cursor: first_page_cursor } = await Events.findAll({
+    account_id,
+    limit: 1,
+  })
+
+  t.truthy(first_page_cursor.created_at)
+  t.is(first_page.length, 1)
+
+  const { rows: second_page, cursor: second_page_cursor } =
+    await Events.findAll({ account_id, limit: 1, cursor: first_page_cursor })
+
+  t.notDeepEqual(first_page[0].client_id, second_page[0].client_id)
+
+  const { rows: third_page, cursor: third_page_cursor } = await Events.findAll({
+    account_id,
+    limit: 1,
+    cursor: second_page_cursor,
+  })
+
+  t.deepEqual(third_page, [])
+  t.falsy(third_page_cursor)
 })
