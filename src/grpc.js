ccad44ee64311bba2b91907297ba205fa4b3ec3b
Yagiz Nizipli <yagiz@nizipli.com>
2021-07-26 18:52:27 +0300

    feat: add mali-ajv bindings

--- src/grpc.js
+++ src/grpc.js
@@ -1,5 +1,6 @@
 import Mali from 'mali'
 import path from 'path'
+import { addSchemas } from 'mali-ajv'
 
 import performancePlugin from './grpc.performance.js'
 import * as Applications from './consumers/applications.js'
@@ -7,6 +8,11 @@ import * as Clients from './consumers/clients.js'
 import * as Events from './consumers/events.js'
 import * as Sessions from './consumers/sessions.js'
 import Logger from './logger.js'
+
+import * as applications from './consumers/applications.schema.js'
+import * as clients from './consumers/clients.schema.js'
+import * as events from './consumers/events.schema.js'
+import * as sessions from './consumers/sessions.schema.js'
 
 const logger = Logger.create().withScope('grpc')
 const options = {
@@ -25,7 +31,7 @@ app.addService(file, ['Applications', 'Clients', 'Events', 'Sessions'], options)
 app.addService(health, 'Health', options)
 
 app.use(performancePlugin)
-
+app.use(addSchemas(app, { applications, clients, events, sessions }))
 app.use({ Applications, Clients, Events, Sessions })
 app.use('grpc.health.v1.Health', 'Check', (ctx) => (ctx.res = { status: 1 }))
 
