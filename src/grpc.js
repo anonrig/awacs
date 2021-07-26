6d2f2d0febc6f354dfbbaa6d07b253492fd45272
Yagiz Nizipli <yagiz@nizipli.com>
2021-07-26 18:49:54 +0300

    refactor: move performance plugin to grpc.performance.js

--- src/grpc.js
+++ src/grpc.js
@@ -1,7 +1,7 @@
 import Mali from 'mali'
 import path from 'path'
-import { PerformanceObserver, performance } from 'perf_hooks'
 
+import performancePlugin from './grpc.performance.js'
 import * as Applications from './consumers/applications.js'
 import * as Clients from './consumers/clients.js'
 import * as Events from './consumers/events.js'
@@ -18,45 +18,13 @@ const options = {
 }
 const file = path.join(path.resolve(''), './src/protofiles/awacs.proto')
 const health = path.join(path.resolve(''), './src/protofiles/health.proto')
-const performanceObserver = new PerformanceObserver((list) => {
-  list.getEntries().forEach((entry) => {
-    logger
-      .withTag('performance')
-      .info(`${entry.name} took ${entry.duration.toFixed(2)} ms`)
-  })
-})
-performanceObserver.observe({ entryTypes: ['measure'], buffered: true })
 
 const app = new Mali()
 
 app.addService(file, ['Applications', 'Clients', 'Events', 'Sessions'], options)
 app.addService(health, 'Health', options)
 
-app.use(async (context, next) => {
-  const isHealthRequest = context.fullName.includes('grpc.health')
-
-  if (isHealthRequest) {
-    return next()
-  }
-
-  performance.mark(context.fullName)
-
-  function measure() {
-    performance.mark(`${context.fullName}-ended`)
-    performance.measure(
-      context.fullName,
-      context.fullName,
-      `${context.fullName}-ended`,
-    )
-  }
-
-  return next()
-    .then(() => measure())
-    .catch((error) => {
-      measure()
-      throw error
-    })
-})
+app.use(performancePlugin)
 
 app.use({ Applications, Clients, Events, Sessions })
 app.use('grpc.health.v1.Health', 'Check', (ctx) => (ctx.res = { status: 1 }))
